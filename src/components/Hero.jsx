10d8fc5bda532b47d950366f7ec5a5b99ceb9ad1
Himanshu Vishwakarma <himanshu1972002@gmail.com>
2026-04-04 20:06:10 +0530

    Added all project files

---
+++ src/components/Hero.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
+
+const Hero = () => {
+  const navigate = useNavigate();
+
+  return (
+    <div
+      className="px-4 sm:px-20 xl:px-32 relative flex flex-col items-center justify-center w-full min-h-screen bg-[url('/gradientBackground.png')] bg-cover bg-no-repeat"
+    >
+      {/* Text Section */}
+      <div className="text-center mb-8">
+        <h1 className="text-4xl md:text-6xl font-bold text-black leading-tight mb-6">
+          Create amazing content <br />
+          with <span className="text-primary">AI tools</span>
+        </h1>
+
+        <p className="mt-4 max-w-xs sm:max-w-md md:max-w-lg lg:max-w-xl mx-auto text-gray-600">
+          Generate articles, blog titles, images, and more with our AI tools.
+          The best way to create content with AI. <br />
+          Transform your ideas into reality.
+        </p>
+      </div>
+
+      {/* Buttons */}
+      <div className="flex flex-wrap justify-center gap-4 text-sm">
+        <button
+          onClick={() => navigate("/ai")}
+          className="bg-primary text-white px-8 py-3 rounded-lg hover:scale-105 active:scale-95 transition duration-200 cursor-pointer"
+        >
+          Start creating now
+        </button>
+
+        <button className="bg-white px-8 py-3 rounded-lg border border-gray-200 hover:scale-105 active:scale-95 transition duration-200 cursor-pointer">
+          Watch demo
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
---
+++ src/pages/Commnity.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Commnity = () => {
+  return (
+    <div>
+        <h1>Commnity</h1>
+    </div>
+  )
+}
+
+export default Commnity
