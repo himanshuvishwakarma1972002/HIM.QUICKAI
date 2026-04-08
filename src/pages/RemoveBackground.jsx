32a91274dfa4731d2fe38ab1c90dc942f4db63a7
Himanshu Vishwakarma <himanshu1972002@gmail.com>
2026-04-08 18:38:31 +0530

    modified:   src/pages/RemoveBackground.jsx         modified:   src/pages/RemoveObject.jsx

--- src/pages/RemoveBackground.jsx
+++ src/pages/RemoveBackground.jsx
@@ -1,9 +1,68 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { EraserIcon, Sparkles } from 'lucide-react'
+import e from 'cors'
 
 const RemoveBackground = () => {
+
+  const [input, setInput] = useState('')
+
+  const onSubmitHandler = (e) => {
+    e.preventDefault()
+    console.log(input, selectedCategory)
+  }
+
   return (
-    <div>
-        <h1>Remove Background</h1>
+<div className='h-full overflow-y-scroll p-6 flex items-start flex-wrap gap-4 text-slate-700'>
+      
+      {/* Left column */}
+      <form 
+        onSubmit={onSubmitHandler}
+        className='w-full max-w-lg p-5 bg-white rounded-lg border border-gray-200 space-y-4'
+      >
+
+        {/* Header */}
+        <div className='flex items-center gap-3'>
+          <Sparkles className='w-6 text-[#FF4938]'/>
+          <h1 className='text-xl font-semibold'>Background Removal</h1>
+        </div>
+
+        {/* Keyword */}
+        <div>
+          <p className='text-sm font-medium'>Upload image</p>
+          <input onChange={(e)=>setInput(e.target.files[0])} type="file" accept='image/*' className='w-full p-2 px-3 mt-2 outline-none text-sm rounded-md border border-gray-300 text-gray-600' required/>
+        </div>
+
+        <p className='text-xs text-gray-500 font-light mt-1'>Supports JPG, PNG, and other image formats</p>
+        
+
+        {/* Submit */}
+        <button
+          type="submit"
+          className='w-full mt-4 bg-[#F6AB41] text-white py-2 rounded-md hover:bg-[#FF4938] transition flex items-center justify-center gap-2'
+        >
+          <EraserIcon className='w-4 h-4'/>
+            Remove Background
+        </button>
+
+      </form>
+
+      {/* Right column */}
+      <div className='w-full max-w-lg p-4 bg-white rounded-lg flex flex-col border border-gray-200 min-h-96'>
+        
+        <div className='flex items-center gap-3'>
+          <EraserIcon className='w-5 h-5 text-[#FF4938]'/>
+          <h1 className='text-xl font-semibold'>Processed Image</h1>
+        </div>
+
+        <div className='flex-1 flex justify-center items-center'>
+          <div className='text-sm flex flex-col items-center gap-5 text-gray-400 text-center'>
+            <EraserIcon className='w-9 h-9'/>
+            <p>Upload an image and click "Remove Background" to get started</p>
+          </div>
+        </div>
+
+      </div>
+
     </div>
   )
 }
