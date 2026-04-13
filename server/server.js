67c943e318a170e7a96346d954c4d36add29db43
Himanshu Vishwakarma <himanshu1972002@gmail.com>
2026-04-13 09:54:23 +0530

    Added frontend and backend to QUICKAI

--- server/server.js
+++ server/server.js
@@ -2,14 +2,20 @@ import express from 'express';
 import cors from 'cors';
 import 'dotenv/config';
 import { clerkMiddleware, getAuth } from '@clerk/express'
+import aiRoutes from './routes/aiRoutes.js';
+import aiRouter from './routes/aiRoutes.js';
+import connectCloudinary from './configs/cloudinary.js';
 
 const app = express();
+
+await connectCloudinary()
 
 app.use(cors());
 app.use(express.json());
 app.use(clerkMiddleware())
 
-app.get('/', (req, res) =>res.send('Server is Live!'));
+app.get('/', (req, res)=>res.send('Server is Live!'));
+app.use('/api/ai', aiRouter);
 
 /** Replaces deprecated requireAuth(): reject requests with no signed-in user. */
 function ensureAuthenticated(req, res, next) {
@@ -24,5 +30,6 @@ app.use(ensureAuthenticated)
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {console.log(`Server is running on port ${PORT}`)});
+app.listen(PORT, () => {console.log('Server is running on port', PORT)})
 export default app;
+
