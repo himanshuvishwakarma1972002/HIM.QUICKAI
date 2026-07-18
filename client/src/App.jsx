2b6f93e7b1121e19928adc32e2f1af6bed0c072f
Himanshu Vishwakarma <himanshu1972002@gmail.com>
2026-07-18 12:46:46 +0530

    fix gpt page import case for Linux build

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -10,7 +10,7 @@ import RemoveBackground from './pages/RemoveBackground'
 import RemoveObject from './pages/RemoveObject'
 import ReviewResime from './pages/ReviewResime'
 import Commnity from './pages/Commnity'
-import Gpt from './pages/gpt.jsx' // ✅ CORRECT
+import Gpt from './pages/Gpt.jsx'
 import { Toaster } from 'react-hot-toast'
 import { useAuth } from '@clerk/react'
 
